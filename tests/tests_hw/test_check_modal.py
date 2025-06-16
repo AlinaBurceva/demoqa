633f0b694c355f1d8a37189615edfedd3f09cf5e
AlinaBurceva <alinaburceva07@ya.ru>
2025-06-16 22:47:33 +0300

    Five Commit

--- tests/tests_hw/test_check_modal.py
+++ tests/tests_hw/test_check_modal.py
@@ -1,13 +1,21 @@
 from pages.modal_dialogs import ModalDialogs
 from conftest import browser
+import requests
+import pytest
 import time
 
 
 
 def test_check_modal(browser):
+    modal_page = ModalDialogs(browser)
+    modal_page.visit()
+
     try:
-        modal_page = ModalDialogs(browser)
-        modal_page.visit()
+        response = requests.get('https://demoqa.com/modal-dialogs')
+        response.raise_for_status()
+
+    except requests.exceptions.RequestException:
+        pytest.skip("Страница недоступна, пропускаем тест")
 
         modal_page.small_modal.click()
         time.sleep(2)
@@ -16,5 +24,4 @@ def test_check_modal(browser):
         modal_page.large_modal.click()
         time.sleep(2)
         modal_page.close_large_modal.click()
-    except:
-        pass
+        time.sleep(2)
